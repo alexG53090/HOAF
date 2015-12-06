71c438b4061a103b69022c51d29015696f17f227
Alex Bennett <alexandergraybennett@gmail.com>
2015-12-06 13:25:32 -0700

    promise chain

--- promises.js
+++ promises.js
@@ -1,11 +1,37 @@
 function myFunc(){
   return new Promise(function(resolve, reject){
-    resolve(2 + 2);
+    resolve(x + 2);
   });
 };
 
 myFunc().then(function(){
   console.log('everything is great!')
-}).catch(function(err){
+}).catch(function(error){
   console.log('everything is not great: ', error)
+})
+
+function addUp(){
+  return new Promise(function(resolve, reject){
+    resolve(3 + 5);
+  });
+};
+
+function subtract(){
+  return new Promise(function(resolve, reject){
+    resolve(20 - 3);
+  });
+};
+
+function divide(){
+  return new Promise(function(resolve, reject){
+    resolve(60 / 5);
+  });
+};
+
+addUp().then(function(){
+  return subtract().then(function(){
+    return divide().then(function(){
+      console.log('done!')
+    })
+  })
 })
